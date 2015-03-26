539271a6cfea8aca0219ad798a26876ab4d3c805
baguspurnomo <bagustebuan@gmail.com>
2015-03-26 23:27:40 +0700

    tugas2

---
+++ tugas 2/soal4.java
@@ -0,0 +1,38 @@
+public class  soal4{
+	public static void main(String[] args) {
+		int tl = 9;
+		switch (tl) {
+		case 1:
+		System.out.print("satu");
+		break;
+		case 2:
+		System.out.print("dua");
+		break;
+		case 3:
+		System.out.print("tiga");
+		break;
+		case 4:
+		System.out.print("empat");
+		break;
+		case 5:
+		System.out.print("lima");
+		break;
+		case 6:
+		System.out.print("enam");
+		break;
+		case 7:
+		System.out.print("tujuh");
+		break;
+		case 8:
+		System.out.print("delapan");
+		break;
+		case 9:
+		System.out.print("sembilan");		
+		break;
+		default:
+		System.out.print("diluar jangkauan");
+			
+		}
+	}
+	
+}
