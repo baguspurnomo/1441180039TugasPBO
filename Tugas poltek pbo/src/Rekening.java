3ed8e5fcfff1bc4fe3642451b9df557acd47e15e
baguspurnomo <bagustebuan@gmail.com>
2015-06-18 22:05:47 +0700

    tugas 8
    1441180039 bagus purnommo

---
+++ Tugas poltek pbo/src/Rekening.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Bagus on 6/17/2015.
+ */
+public class Rekening {
+    private Double SaldoRekening;
+
+    public Rekening(Double SaldoRekening) {this.SaldoRekening = SaldoRekening; }
+
+
+    public Double getSaldoRekening() { return SaldoRekening; }
+
+    public void setSaldoRekening(Double saldoRekening) {this.SaldoRekening = SaldoRekening;}
+
+    public void tarik (double tarik){
+        // untuk mengurangi nilai saldo
+        this.SaldoRekening = SaldoRekening - tarik;
+
+
+    }
+
+    public void  setor (double setor) {
+        // unuk menambah nilai saldo
+        this.SaldoRekening = SaldoRekening + setor;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Rekening{" +
+                "SaldoRekening=" + SaldoRekening +
+                '}';
+    }
+}
---
+++ Tugas poltek pbo/src/TestATM.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Bagus on 6/17/2015.
+ */
+public class TestATM {
+    public static void main(String[] args) {
+//      buat objek rekening
+
+        Rekening aa = new Rekening (2000000.0);
+        Nasabah bb = new Nasabah("Bagus","Banyuwangi");
+        System.out.println(bb.toString());
+        bb.setRekeningNasabah(aa);
+        System.out.println(bb.toString());
+
+//tarik saldo sebesar 10 juta
+  aa.tarik(10000000.0);
+//tampilkan saldo sekarang
+        System.out.println("Tarik sebesar 10.000.000");
+        System.out.println("Saldo anda sekarang = " +aa.getSaldoRekening());
+//setor saldo sebesar 5 juta
+  aa.setor(5000000.0);
+//tampilkan saldo sekarang
+        System.out.println("Menabung sebesar Rp. 5.000.000");
+        System.out.println("Saldo anda sekarang =" +aa.getSaldoRekening());
+    }
+
+}
